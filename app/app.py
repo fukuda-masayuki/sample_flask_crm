e81ab46852b0af40df941190e2e0cd2432f8cc60
fukuda-masayuki <fm1146274.fm@gmail.com>
2023-04-20 23:38:36 +0900

    DBを扱うための処理を買い追加

--- app/app.py
+++ app/app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class CTest(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.Integer)
 
 @app.route("/")
 def index():
-    return "test"
+    return "testh"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
